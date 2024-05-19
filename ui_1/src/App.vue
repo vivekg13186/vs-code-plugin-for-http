2b3dd5f2221d8a4aa9d43bb2fa5914cae3670e87
vivek <vgangadharan@you-get.com>
2024-05-19 19:46:37 +0200

    base

---
+++ ui_1/src/App.vue
@@ -0,0 +1,108 @@
+<script setup >
+import {
+  provideVSCodeDesignSystem,
+  vsCodeButton,
+  vsCodePanelTab,
+  vsCodePanels,
+  vsCodePanelView,
+  vsCodeDivider,
+  vsCodeDataGridRow,
+  vsCodeDataGridCell,
+  vsCodeTextField,
+  vsCodeCheckbox,
+  vsCodeDataGrid,
+  vsCodeTextArea,
+  vsCodeDropdown,
+  vsCodeOption
+} from "@vscode/webview-ui-toolkit";
+provideVSCodeDesignSystem().register(
+  vsCodeButton(),
+  vsCodePanelTab(),
+  vsCodePanels(),
+  vsCodePanelView(),
+  vsCodeDivider(),
+  vsCodeDataGridRow(),
+  vsCodeDataGridCell(),
+  vsCodeDataGrid(),
+  vsCodeTextField(),
+  vsCodeCheckbox(),
+  vsCodeTextArea(),
+  vsCodeDropdown(),
+  vsCodeOption()
+);
+
+import {ref,onMounted} from "vue";
+import KeyValue from "./components/KeyValue.vue"
+import Iblize from "iblize";
+
+const preeditor = ref(null);
+const posteditor = ref(null);
+var preeditor_obj = null;
+var posteditor_obj = null;
+onMounted(()=>{
+  preeditor_obj = new Iblize(preeditor.value, {
+    language: "javascript",
+  });
+  posteditor_obj = new Iblize(posteditor.value, {
+    language: "javascript",
+  });
+})
+
+</script>
+
+<template>
+  <div class="toolbar">
+    <vscode-dropdown value="GET">
+      <vscode-option>GET</vscode-option>
+      <vscode-option>POST</vscode-option>
+      <vscode-option>PUT</vscode-option>
+      <vscode-option>PATCH</vscode-option>
+      <vscode-option>DELETE</vscode-option>
+      <vscode-option>HEAD</vscode-option>
+      <vscode-option>OPTIONS</vscode-option>
+    </vscode-dropdown>
+    <vscode-text-field type="text" ref="host" class="urltext" placeholder="http://www.google.com"></vscode-text-field>
+    <vscode-button appearance="primary">SEND</vscode-button>
+    <vscode-button appearance="primary">SAVE</vscode-button>
+  </div>
+
+  <div class="row1">
+    <vscode-panels>
+      <vscode-panel-tab id="tab-1">Params</vscode-panel-tab>
+      <vscode-panel-tab id="tab-2">Authorisation</vscode-panel-tab>
+      <vscode-panel-tab id="tab-3">Headers</vscode-panel-tab>
+      <vscode-panel-tab id="tab-4">Body</vscode-panel-tab>
+      <vscode-panel-tab id="tab-5">
+Pre Script
+      </vscode-panel-tab>
+      <vscode-panel-tab id="tab-6">
+Post Script
+      </vscode-panel-tab>
+      <vscode-panel-view id="view-1">
+        <KeyValue></KeyValue>
+      </vscode-panel-view>
+      <vscode-panel-view id="view-2">Authorisation</vscode-panel-view>
+      <vscode-panel-view id="view-3">
+        <KeyValue></KeyValue>
+      </vscode-panel-view>
+      <vscode-panel-view id="view-4">Body</vscode-panel-view>
+      <vscode-panel-view id="view-5">
+        <div id="preeditor" ref="preeditor"></div>
+      </vscode-panel-view>
+      <vscode-panel-view id="view-6">
+        <div id="posteditor" ref="posteditor"></div>
+      </vscode-panel-view>
+    </vscode-panels>
+  </div>
+  <vscode-divider></vscode-divider>
+
+</template>
+<style scoped>
+#preeditor,#posteditor{
+    width: 100%;
+      height: 350px;
+
+}
+</style>
+
+ 
---
+++ ui_1/src/components/KeyValue.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <div class="tools">
+            <vscode-button appearance="primary" @click="toogleEdit">Edit</vscode-button>
+        </div>
+
+        <vscode-data-grid aria-label="Basic" v-if="edit==false">
+            <vscode-data-grid-row row-type="header">
+                <vscode-data-grid-cell cell-type="columnheader" grid-column="1">Key</vscode-data-grid-cell>
+                <vscode-data-grid-cell cell-type="columnheader" grid-column="2">Value</vscode-data-grid-cell>
+            </vscode-data-grid-row>
+            <vscode-data-grid-row>
+                <vscode-data-grid-cell grid-column="1">Cell Data</vscode-data-grid-cell>
+                <vscode-data-grid-cell grid-column="2">Cell Data</vscode-data-grid-cell>
+            </vscode-data-grid-row>
+            <vscode-data-grid-row>
+                <vscode-data-grid-cell grid-column="1">Cell Data</vscode-data-grid-cell>
+
+                <vscode-data-grid-cell grid-column="2">Cell Data</vscode-data-grid-cell>
+            </vscode-data-grid-row>
+            <vscode-data-grid-row>
+                <vscode-data-grid-cell grid-column="1">Cell Data</vscode-data-grid-cell>
+
+                <vscode-data-grid-cell grid-column="2">Cell Data</vscode-data-grid-cell>
+            </vscode-data-grid-row>
+        </vscode-data-grid>
+        <vscode-text-area  class="editor" rows="10" v-if="edit==true"></vscode-text-area>
+    </div>
+
+</template>
+<script setup>
+import {ref} from "vue";
+
+const edit = ref(false)
+const toogleEdit = ()=>{
+    edit.value = !edit.value;
+}
+</script>
+<style scoped>
+.tools{
+    display: flex;
+    width: 100%;
+    align-items: flex-end;
+}
+.container{
+    width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        gap:10px;
+}
+.editor{
+    width: 100%;
+    height: 350px;
+}
+</style>
